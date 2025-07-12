393d3f42cd9c5c1c2f7c748388c4a7ea908cb1fb
Eyecta Studio <152719844+eyecta@users.noreply.github.com>
2025-07-13 00:08:34 +0200

    Add qa-platform/templates/footer.php 1752358114172

---
+++ qa-platform/templates/footer.php
@@ -0,0 +1,7 @@
+</main>
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> <?php echo htmlspecialchars(SITE_NAME); ?>. All rights reserved.</p>
+</footer>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
